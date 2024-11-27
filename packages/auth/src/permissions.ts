ee01beb577f164c9e8fd29b60103f50af45b292d
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2024-11-27 00:04:22 -0300

    [feat] add base permission config using zod and casl;

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,33 @@
+import { AbilityBuilder } from '@casl/ability';
+import { AppAbility } from '.';
+import { User } from './models/user';
+import { Role } from './roles';
+
+type PermissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>
+) => void;
+
+export const permissions: Record<Role, PermissionsByRole> = {
+  ADMIN: (user, builder) => {
+    const { can, cannot } = builder;
+
+    can('manage', 'all');
+    cannot(['transfer_ownership', 'update'], 'Organization');
+    can(['transfer_ownership', 'update'], 'Organization', {
+      ownerId: { $eq: user.id },
+    });
+  },
+  MEMBER: (user, builder) => {
+    const { can } = builder;
+
+    can('get', 'User');
+    can(['create', 'get'], 'Project');
+    can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } });
+  },
+  BILLING: (_, builder) => {
+    const { can } = builder;
+
+    can('manage', 'Billing');
+  },
+};
