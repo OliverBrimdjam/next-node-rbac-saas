ad6add8daa12234330a59aad50629ea357b19ff7
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2024-11-27 22:23:58 -0300

    [feat] implements createAccount route registration;

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -5,6 +5,7 @@ import {
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod';
+import { createAccount } from './routes/auth/create-account';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -12,6 +13,8 @@ app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifyCors);
+
+app.register(createAccount);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Http server running!');
